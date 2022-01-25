1c34efc7bc8d5ad2ef5ddfdfa29b456d210d8c55
Chathan Driehuys <chathan@driehuys.com>
2022-01-25 19:01:34 +0000

    chore: Set up build script.
    Add a build script to generate production static files.

--- build.ts
+++ build.ts
@@ -2,10 +2,64 @@
  * Build script for the zeroed books web app.
  */
 
+import { promises as fs } from "fs";
+import path from "path";
+
 import * as esbuild from "esbuild";
 
 const PUBLIC_DIR = "public";
-const BUILD_DIR = "public/dist";
+const BUNDLE_DIR = "js";
+const BUILD_DIR = "dist";
+
+const ENTRY_POINTS = ["src/index.tsx"];
+
+const rmDir = async (target: string): Promise<void> => {
+    await fs.rm(target, { force: true, recursive: true });
+};
+
+const copyDir = async (src: string, dest: string): Promise<void> => {
+    await fs.mkdir(dest, { recursive: true });
+    const entries = await fs.readdir(src, { withFileTypes: true });
+
+    for (const entry of entries) {
+        let srcPath = path.join(src, entry.name);
+        let destPath = path.join(dest, entry.name);
+
+        if (entry.isDirectory()) {
+            console.log("Recursing into directory:", srcPath);
+            await copyDir(srcPath, destPath);
+        } else {
+            console.log(`Copying '${srcPath}' to '${destPath}'`);
+            await fs.copyFile(srcPath, destPath);
+        }
+    }
+}
+
+const build = async (): Promise<void> => {
+    try {
+        console.log("Cleaning existing build directory...");
+        await rmDir(BUILD_DIR);
+        console.log("Build directory cleaned.\n");
+
+        console.log("Copying public files to build directory...");
+        await copyDir(PUBLIC_DIR, BUILD_DIR);
+        console.log("Public files copied.\n");
+
+        console.log("Building with esbuild...");
+        await esbuild.build({
+            bundle: true,
+            entryPoints: ENTRY_POINTS,
+            logLevel: "info",
+            minify: true,
+            outdir: `${BUILD_DIR}/${BUNDLE_DIR}`,
+            sourcemap: true,
+        });
+        console.log("Build complete.\n");
+    } catch (error) {
+        console.error(error);
+        process.exit(1);
+    }
+};
 
 const serveRequestHandler = (request: esbuild.ServeOnRequestArgs) => {
     console.log(`${request.remoteAddress} - "${request.method} ${request.path}" ${request.status} [${request.timeInMS}ms]`);
@@ -26,14 +80,14 @@ const serve = async (): Promise<void> => {
             },
             {
                 bundle: true,
-                entryPoints: ["src/index.tsx"],
+                entryPoints: ENTRY_POINTS,
                 logLevel: "info",
-                outdir: BUILD_DIR,
+                outdir: `${PUBLIC_DIR}/${BUNDLE_DIR}`,
                 sourcemap: true,
             }
         )
-    } catch {
-        // Esbuild handles logging of the error, so we can just exit.
+    } catch (error) {
+        console.error(error);
         process.exit(1);
     }
 };
@@ -45,10 +99,18 @@ const serve = async (): Promise<void> => {
  * element array containing the build command.
  */
 const run = (args: string[]) => {
-    if (args[0] === "serve") {
-        serve();
-    } else {
-        console.error("Unrecognized argument:", args[0]);
+    switch (args[0]) {
+        case undefined:
+            build();
+            break;
+
+        case "serve":
+            serve();
+            break;
+
+        default:
+            console.error("Unrecognized argument:", args[0]);
+            process.exit(1);
     }
 }
 
