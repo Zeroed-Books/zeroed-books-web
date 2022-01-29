2c7837b5ffc1bc0ed76a03204aac8e5861d3e45b
Chathan Driehuys <chathan@driehuys.com>
2022-01-29 00:03:32 +0000

    feat: Persist current auth status.

--- src/authentication/useAuthStatus.tsx
+++ src/authentication/useAuthStatus.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useState } from "react";
+import React, { createContext, useContext, useEffect, useState } from "react";
 import { useQuery } from "react-query";
 import { getAuthStatus } from "./api";
 
@@ -12,16 +12,31 @@ const AuthContext = createContext<AuthContextData>({
   setAuthenticated: () => {},
 });
 
+// Pull the initial status from local storage so we don't get a redirect to the
+// login page followed by an immediate redirect back to where we were on each
+// page load.
+const authStorageKey = "isAuthenticated";
+const initialAuthStatus =
+  window.localStorage.getItem(authStorageKey) === "true";
+
 export const AuthProvider: React.FC = ({ children }) => {
-  const [isAuthenticated, setAuthenticated] = useState(false);
+  const [isAuthenticated, setAuthenticated] = useState(initialAuthStatus);
   const value = { isAuthenticated, setAuthenticated };
 
-  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
-};
+  // Cache the current authentication status to local storage so we can use it
+  // as the initial state for the next page load.
+  useEffect(() => {
+    try {
+      window.localStorage.setItem(authStorageKey, isAuthenticated.toString());
+    } catch (e) {
+      // We don't really care that the caching operation failed because it just
+      // means the auth status won't be immediately available on the next page
+      // load, and we'll have to wait for the query to go through.
+      console.debug("Failed to persist authentication status:", e);
+    }
+  }, [isAuthenticated]);
 
-const useAuthStatus = () => {
-  const { isAuthenticated, setAuthenticated } = useContext(AuthContext);
-  const authStatusQuery = useQuery(["authentication", "me"], getAuthStatus, {
+  useQuery(["authentication", "me"], getAuthStatus, {
     onError: () => {
       setAuthenticated(false);
     },
@@ -31,6 +46,12 @@ const useAuthStatus = () => {
     retry: false,
     staleTime: 60_000,
   });
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+};
+
+const useAuthStatus = () => {
+  const { isAuthenticated } = useContext(AuthContext);
 
   return { isAuthenticated };
 };
