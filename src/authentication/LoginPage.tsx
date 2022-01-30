87da741f9d9cd3a11b757f3c3b847ebeec1d3abf
Chathan Driehuys <chathan@driehuys.com>
2022-01-30 03:35:13 +0000

    chore: Add tsconfig file.
    Enable strict mode so we get better hints in the editor.

--- src/authentication/LoginPage.tsx
+++ src/authentication/LoginPage.tsx
@@ -16,7 +16,7 @@ const LoginPage = () => {
   const authStatus = useAuthStatus();
   const location = useLocation();
 
-  let nextURL = (location.state as PageState)?.from.pathname ?? "/";
+  let nextURL = (location.state as PageState)?.from?.pathname ?? "/";
 
   const queryClient = useQueryClient();
   const loginMutation = useMutation<void, AxiosError, Credentials>(
