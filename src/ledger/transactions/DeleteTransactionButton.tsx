4dac126782fce6f08b50b325409638ab36f85d8c
Chathan Driehuys <chathan@driehuys.com>
2022-01-30 03:46:09 +0000

    feat: Query invalidation for transaction delete.

--- src/ledger/transactions/DeleteTransactionButton.tsx
+++ src/ledger/transactions/DeleteTransactionButton.tsx
@@ -1,11 +1,12 @@
 import { Button } from "@mantine/core";
-import { useToggle } from "@mantine/hooks";
 import { useNotifications } from "@mantine/notifications";
-import { TrashIcon } from "@modulz/radix-icons";
+import { CrossCircledIcon, TrashIcon } from "@modulz/radix-icons";
+import { AxiosError } from "axios";
 import React from "react";
-import { useMutation } from "react-query";
+import { useMutation, useQueryClient } from "react-query";
 import { Navigate } from "react-router-dom";
-import { deleteTransaction, Transaction } from "./api";
+import { deleteTransaction, ResourceCollection, Transaction } from "./api";
+import { transactionKeys } from "./queries";
 
 interface Props {
   transaction: Transaction;
@@ -13,16 +14,47 @@ interface Props {
 
 const DeleteTransactionButton: React.FC<Props> = ({ transaction }) => {
   const notifications = useNotifications();
-  const mutation = useMutation(async () => deleteTransaction(transaction.id), {
-    onSuccess: () => {
-      notifications.showNotification({
-        color: "red",
-        icon: <TrashIcon />,
-        message: `Transaction for "${transaction.payee}" has been deleted.`,
-        title: "Transaction Deleted",
-      });
-    },
-  });
+
+  const queryClient = useQueryClient();
+  const mutation = useMutation<void, AxiosError>(
+    async () => deleteTransaction(transaction.id),
+    {
+      onError: (error) => {
+        const message =
+          error.response?.data?.message ?? "Something went wrong.";
+        notifications.showNotification({
+          color: "red",
+          icon: <CrossCircledIcon />,
+          message,
+        });
+      },
+
+      onSettled: () => {
+        queryClient.invalidateQueries(transactionKeys.all);
+      },
+
+      onSuccess: () => {
+        notifications.showNotification({
+          color: "red",
+          icon: <TrashIcon />,
+          message: `Transaction for "${transaction.payee}" has been deleted.`,
+          title: "Transaction Deleted",
+        });
+
+        // Remove the deleted transaction from the query data so it disappears
+        // without requiring a refetch.
+        const transactionList = queryClient.getQueryData<
+          ResourceCollection<Transaction>
+        >(transactionKeys.list());
+        if (transactionList) {
+          const updatedTransactions = transactionList.items.filter(
+            (t) => t.id !== transaction.id
+          );
+          queryClient.setQueryData(transactionKeys.list(), updatedTransactions);
+        }
+      },
+    }
+  );
 
   if (mutation.isSuccess) {
     return <Navigate to="/" />;
